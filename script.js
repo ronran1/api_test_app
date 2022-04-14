32205f5aac4d0801f5a711cd731d9aee70573c8d
Ron Ran <rohan.rani5444@gmail.com>
2022-04-13 18:50:31 -0700

    created initial functionality from API

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector('button').addEventListener('click', getStockPrices)
+console.log(document.getElementsByTagName('li'))
+function getStockPrices() {
+    let choice = document.querySelector('input').value
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${choice}&apikey=G0FXED0O9KX71PZX`)
+        .then(response => response.json())
+        .then(data => {
+        console.log(data);
+        // console.log(data["Time Series (60min)"])
+        // console.log(Object.values(data["Weekly Time Series"])[0])
+        console.log(JSON.stringify(Object.values(data["Weekly Time Series"])[0]))
+        for (i = 0; i < document.getElementsByTagName('li').length; i++) {
+            document.querySelectorAll('li')[i].innerHTML = JSON.stringify(Object.values(data["Weekly Time Series"])[i])
+        }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
