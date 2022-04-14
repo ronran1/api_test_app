26b66cc6802acf73642cab15b8d6e1094e26ad4f
Ron Ran <rohan.rani5444@gmail.com>
2022-04-13 21:10:36 -0700

    added dates to weekly times

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function getStockPrices() {
         console.log(data);
         // console.log(data["Time Series (60min)"])
         // console.log(Object.values(data["Weekly Time Series"])[0])
-        console.log(JSON.stringify(Object.values(data["Weekly Time Series"])[0]))
+        // console.log(JSON.stringify(Object.keys(data["Weekly Time Series"])[0]));
         for (i = 0; i < document.getElementsByTagName('li').length; i++) {
-            document.querySelectorAll('li')[i].innerHTML = JSON.stringify(Object.values(data["Weekly Time Series"])[i])
+            document.querySelectorAll('li')[i].innerHTML = `${JSON.stringify(Object.keys(data["Weekly Time Series"])[i])}: ${JSON.stringify(Object.values(data["Weekly Time Series"])[i])}`
         }
   })
   .catch(error => {
